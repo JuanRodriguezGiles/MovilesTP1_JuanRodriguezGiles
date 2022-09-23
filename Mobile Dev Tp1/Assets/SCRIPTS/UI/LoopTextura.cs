859d39093a87ab60d20bf0d4a1d5a9ef259505ca
Juan <juanrodriguezgiles@gmail.com>
2022-09-23 11:58:12 -0300

    Update unload scene images.

--- Mobile Dev Tp1/Assets/SCRIPTS/UI/LoopTextura.cs
+++ Mobile Dev Tp1/Assets/SCRIPTS/UI/LoopTextura.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 public class LoopTextura : MonoBehaviour
 {
     public float Intervalo = 1;
+    public int player;
 
-    public Texture2D[] Imagenes;
+    public Texture2D[] mobileImages;
+    public Texture2D[] player1Images;
+    public Texture2D[] player2Images;
     private int Contador;
     private float Tempo;
 
     // Use this for initialization
     private void Start()
     {
-        if (Imagenes.Length > 0)
-            GetComponent<Renderer>().material.mainTexture = Imagenes[0];
+#if UNITY_STANDALONE
+        GetComponent<Renderer>().material.mainTexture = player == 1 ? player1Images[0] : player2Images[0];
+#elif UNITY_ANDROID
+        GetComponent<Renderer>().material.mainTexture = mobileImages[0];
+#endif
     }
 
-    // Update is called once per frame
     private void Update()
     {
         Tempo += Time.deltaTime;
@@ -24,8 +29,13 @@ public class LoopTextura : MonoBehaviour
         {
             Tempo = 0;
             Contador++;
-            if (Contador >= Imagenes.Length) Contador = 0;
-            GetComponent<Renderer>().material.mainTexture = Imagenes[Contador];
+            if (Contador >= player1Images.Length) Contador = 0;
+            
+#if UNITY_STANDALONE
+            GetComponent<Renderer>().material.mainTexture = player == 1 ? player1Images[Contador] : player2Images[Contador];
+#elif UNITY_ANDROID
+        GetComponent<Renderer>().material.mainTexture = mobileImages[Contador];
+#endif
         }
     }
 }
